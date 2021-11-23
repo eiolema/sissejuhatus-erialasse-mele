e3c778f48e2337e3f940fb258db1a82e243ed464
eiolema <eiolema@users.noreply.github.com>
2021-11-23 22:51:34 +0200

    Faraday (#4)
    * faraday teema sisu
    
    * faraday teema sisu
    w3 html ja css valideerimise fixid
    
    Co-authored-by: merit <merit@bitweb.ee>

--- assets/main.js
+++ assets/main.js
@@ -1,18 +1,25 @@
-const ytButtons = document.querySelectorAll(".yt-button");
-ytButtons.forEach(button => button.addEventListener("click", event => openModal(event)));
+const ytButtons = document.querySelectorAll(".yt-button"); // yt linkidele on lisatud klass "yt-button, mille järgi on võimalik nüüd õiged elemendid üles leida"
+ytButtons.forEach(button => button.addEventListener("click", event => openModal(event))); //kõigile youtube linkidele lisatakse click listener
 
-document.getElementById("close-btn").addEventListener("click", event => closeModal());
+document.getElementById("close-btn").addEventListener("click", event => closeModal()); // modaali sulgemisnupule lisatake click listener
 
+//modaali avamise funktsioon
 function openModal(event) {
-    event.preventDefault();
-    let url = event.target.getAttribute("data-yt-url");
-    document.getElementById("yt-modal").style.display = "block";
-    document.getElementById("yt-iframe").setAttribute("src", url);
+    event.preventDefault(); //takistab lingi vajutamisel lehe üles kerimist
+    let url = event.target.getAttribute("data-yt-url"); // parameetri väärtuseks antakse custom attribuudi "data-yt-url" väärtus
+    document.getElementById("yt-modal").style.display = "block"; // modaali div tehakse nähtavaks
+
+    let ytIframe = document.getElementById("yt-iframe");
+    ytIframe.setAttribute("src", url);//youtube videot sisaldavale iframele antakse ette video url
+    ytIframe.setAttribute("height", "500");// iframe kõrgus
+    ytIframe.setAttribute("width", "100%");//iframe laius
+
 }
 
+// modaali sulgemise funktsioon
 function closeModal() {
-    document.getElementById("yt-modal").style.display = "none";
-    document.getElementById("yt-iframe").setAttribute("src", "");
+    document.getElementById("yt-modal").style.display = "none"; //modaali sulgemisel peidetakse selle div
+    document.getElementById("yt-iframe").setAttribute("src", ""); // yt videot sisaldava iframe src attribuuti väärtus nullitakse ära
 }
 
 
